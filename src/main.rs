57903220264edc66ba4f4fb9363790dfdcfce80d
peg <ameba23@systemli.org>
2024-09-07 15:22:49 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+/// For explanation see https://www.kernel.org/doc/Documentation/ABI/testing/configfs-tsm
+use std::{
+    fs::{create_dir, File},
+    io::{Read, Result, Write},
+    path::PathBuf,
+};
+
+struct OpenQuote {
+    path: PathBuf,
+}
+
+impl OpenQuote {
+    fn new(quote_name: &str) -> Result<Self> {
+        // In prod this would be /sys/kernel/config/tsm/report
+        let mut quote_path = PathBuf::from("./temp");
+        quote_path.push(quote_name);
+        create_dir(quote_path.clone())?;
+        Ok(Self { path: quote_path })
+    }
+
+    fn write_input(&self, input: [u8; 64]) -> Result<()> {
+        let mut inblob_path = self.path.clone();
+        inblob_path.push("inblob");
+        let mut inblob_file = File::create(inblob_path)?;
+        inblob_file.write_all(&input)?;
+        Ok(())
+    }
+
+    fn read_output(&self) -> Result<Vec<u8>> {
+        let mut outblob_path = self.path.clone();
+        outblob_path.push("outblob");
+        let mut outblob_file = File::open(outblob_path)?;
+        let mut output = Vec::new();
+        outblob_file.read_to_end(&mut output)?;
+        Ok(output)
+    }
+}
+
+fn main() {
+    let quote_name = std::env::args().nth(1).unwrap_or("test-quote".to_string());
+    let quote = OpenQuote::new(&quote_name).unwrap();
+    quote.write_input([0; 64]).unwrap();
+    let output = quote.read_output().unwrap();
+    println!("Output: {:?}", output);
+}
