965d72770311c3bd7143114060230c9ed45bc2df
peg <ameba23@systemli.org>
2024-10-02 08:43:05 +0200

    Add docs / prepare for publish

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # configfs-tsm
 
-Generate quotes for TDX (or potentially other confidential computing platforms) using linux [configfs-tsm](https://www.kernel.org/doc/Documentation/ABI/testing/configfs-tsm)
+This can generate quotes for remote attestation on confidential computing platforms using Linux's
+[configfs-tsm](https://www.kernel.org/doc/Documentation/ABI/testing/configfs-tsm) filesystem
+interface.
 
-Warning: This is a work-in-progress and has not been audited
+This is designed for and tested with Intel TDX, but since the `configfs-tsm` is a platform-agnostic
+interface, this could potentially work with other platforms such as Intel SGX, or AMD SEV.
+
+This crate has no dependencies and generates quotes only by reading and writing local files.
+
+Warning: This crate is in early stages of development and has not been audited
--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,17 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//! For explanation see https://www.kernel.org/doc/Documentation/ABI/testing/configfs-tsm
 
+//! This can generate quotes for remote attestation on confidential computing platforms using Linux's
+//! [configfs-tsm](https://www.kernel.org/doc/Documentation/ABI/testing/configfs-tsm) filesystem
+//! interface.
+//!
+//! This is designed for and tested with Intel TDX, but since the `configfs-tsm` is a platform-agnostic
+//! interface, this could potentially work with other platforms such as Intel SGX, or AMD SEV.
+//!
+//! This crate has no dependencies and generates quotes only by reading and writing local files.
+//!
+//! Warning: This crate is in early stages of development and has not been audited
 use std::{
     fmt::{self, Display},
     fs::{create_dir, read_to_string, File},
